fa8c0185411c445d77d4fa34e3c0e56fc5c66dd0
Keano Robinson <keano@Keanos-MBP.home>
2021-07-29 16:48:17 +0100

    Update App.js to display a demo of the component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
 import StarRating from "./StarRating";
+import { makeStar } from "./Star";
+
+import { BiBaseball } from "react-icons/bi";
+
+/* 
+An example of creating a custom 'star' using another fillable svg icon.
+Here the default icon is overridden from a star icon to a baseball icon, and the color of the icon when it is active (i.e. selected)
+is overridden from yellow to red, and the size of the icon is overriden from 20px to 50px. The inactive color of the icon is left as default.
+This is used to override the default value of StarComponent to the new 'star' icon.
+*/
+const Baseball = makeStar({icon: BiBaseball, activeColor: "red", size: {value:"50", unit:"px"}});
 
 export default function App() {
-    return <>{StarRating()}</>;
+    const [rating, setRating] = useState(4.5); //Give the app an inital rating of 4.5 for demonstration purposes
+    const totalStars = 5;
+    return (
+        <div className="app-container">
+            {StarRating({
+                initalRating: rating,
+                totalStars,
+                clickable: true,
+                clickHandler: (newRating, _totalStars) => setRating(newRating),
+                StarComponent: Baseball
+            })}
+            <p>
+                Rating: {rating.toFixed(1)} out of {totalStars.toFixed(1)}
+            </p>
+        </div>
+    );
 }
