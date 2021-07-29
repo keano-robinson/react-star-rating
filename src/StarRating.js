c1fb41e9d5d0f2c5b837496999877e73369c6461
Keano Robinson <keano@Keanos-MBP.home>
2021-07-29 16:52:13 +0100

    Update StarRating component to interactively set ratings

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,14 +1,40 @@
-import React from "react";
-import { Star, makeStar } from "./Star.js";
+import React, { useState } from "react";
 
-export default function StarRating({ totalStars = 5, rating = 2.2 } = {}) {
+import { Star } from "./Star.js";
+
+
+export default function StarRating({ totalStars = 5, initalRating = 0, clickable = false, clickHandler = f => f, StarComponent=Star } = {}) {
+    //Using React's useState hook to manage state
+    const [rating, setRating] = useState(initalRating);
+
     /*Function to turn a rating into an array of values for each star 
     (e.g. (totalStars=5, rating=3.3) --> [1, 1, 1, 0.3, 0, 0]) of total stars*/
-    const createArray = (totalStars, rating) => [...Array(Math.floor(rating)).fill(1), rating % 1, ...Array(Math.floor(totalStars-rating)).fill(0)];
+    const createArray = (totalStars, rating) => [
+        ...Array(Math.floor(rating)).fill(1), 
+        ...Array(Math.ceil(rating % 1)).fill(rating % 1), 
+        ...Array(Math.floor(totalStars-rating)).fill(0)
+    ];
+
+    const handleRating = (rating) => {
+        if (clickable) {
+            clickHandler(rating, totalStars);
+            setRating(rating);
+        } else {
+            //do nothing
+        }
+    }
 
     return (
-        <div className="star-rating" title={rating}>
-            {createArray(totalStars, rating).map((v, i) => <Star key={i} value={v} />)}
-        </div>
+        <>
+            <div className="star-rating" title={rating.toFixed(1)}>
+                {
+                    createArray(totalStars, rating).map((v, i) => 
+                        <StarComponent 
+                            key={i} 
+                            value={v} 
+                            onSelect={() => handleRating(i + 1)} />)
+                }
+            </div>
+        </>
     );
 }
