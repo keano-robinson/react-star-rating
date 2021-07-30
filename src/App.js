11850562853d6224a6714742aaa1019bcba38cd1
Keano Robinson <keano@Keanos-MBP.home>
2021-07-30 12:49:25 +0100

    Update comments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 
-import StarRating from "./StarRating";
-import { makeStar } from "./Star";
+import StarRating from "./StarRating"; //Import StarRating component
+import { makeStar } from "./Star"; //makeStar allows developers to make a custom 'star' icon
 
-import { IoMdPlanet } from "react-icons/io";
+import { IoMdPlanet } from "react-icons/io"; //Import a fillable svg from react-icons to make a custom 'star'
 
 /* 
     An example of creating a custom 'star' using a fillable svg icon from react-icons.
     Here, the default icon is overridden from a star icon to a planet icon. The color of the icon when it is active (i.e. selected)
     is overridden from yellow to red. And the size of the icon is overriden from 20px to 50px.
 
-    All other parameters are left as default: The inactive color of the icon and the number of discrete divisions of the icon.
+    All other parameters are left as default: 
+     - The inactive color of the icon (grey) 
+     - The number of discrete divisions of the icon (4 divisions by default, indicating that the user can adjust ratings in increments of 0.25)
 
     This is used to override the default value of StarComponent to the new 'star' icon, a baseball icon.
 */
 
-const Planet = makeStar({icon: IoMdPlanet, activeColor: "red", size: {value:"50", unit:"px"}});
+const Planet = makeStar({icon: IoMdPlanet, activeColor: "red", size: {value:"50", unit:"px"}, numberOfDivisions: 4});
 
 export default function App() {
     const [rating, setRating] = useState(4.5); //Give the app an inital rating of 4.5 for demonstration purposes
@@ -24,12 +26,13 @@ export default function App() {
     return (
         <div className="app-container">
             {StarRating({
-                initalRating: rating,
-                totalStars,
-                clickable: true,
-                clickHandler: (newRating, _totalStars) => setRating(newRating),
-                StarComponent: Planet,
-                decimalPrecision
+                initalRating: rating, //0 by default
+                totalStars, //5 by default
+                clickable: true, //Makes StarRating interactive (false by default)
+                clickHandler: (newRating, _totalStars) => setRating(newRating), //Dynamically adjusts rating. These values used in the paragraph below.
+                     //When StarRating is clicked, clickHandler is called with the new rating and total number of 'stars' of the StarRating component
+                StarComponent: Planet, //A gold star with grey background by default
+                decimalPrecision //Adjusts the precision of the tooltip
             })}
             <p>
                 Rating: {rating.toFixed(decimalPrecision)} out of {totalStars.toFixed(decimalPrecision)}
